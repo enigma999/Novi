afbb9068d20dfa68cd292912648ed5f4024709ea
Stan <stan.borst@storekeeper.com>
2026-08-27 20:16:23 +0100

    added type

--- backend-java-pokemon-interface/src/FirePokemon.java
+++ backend-java-pokemon-interface/src/FirePokemon.java
@@ -1,5 +1,5 @@
 public class FirePokemon extends Pokemon {
     public FirePokemon(String name, int level, int hp, String food, String sound) {
-        super(name, level, hp, food, sound);
+        super(name, level, hp, food, sound, "fire");
     }
 }
--- backend-java-pokemon-interface/src/Pokemon.java
+++ backend-java-pokemon-interface/src/Pokemon.java
@@ -4,12 +4,14 @@ public class Pokemon {
     private int hp;
     private final String food;
     private final String sound;
-    public Pokemon(String name, int level, int hp, String food, String sound){
+    private final String type;
+    public Pokemon(String name, int level, int hp, String food, String sound, String type){
         this.name = name;
         this.level = level;
         this.hp = hp;
         this.food = food;
         this.sound = sound;
+        this.type = type;
     }
 
     public String getName() {
@@ -30,5 +32,9 @@ public class Pokemon {
 
     public String getSound() {
         return sound;
+    }
+
+    public String getType() {
+        return type;
     }
 }
