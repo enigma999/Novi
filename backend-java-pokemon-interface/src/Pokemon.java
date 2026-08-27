d3aac9aef25bb488cd3a3926a03fe1183c193e05
Stan <stan.borst@storekeeper.com>
2026-08-27 19:17:07 +0100

    build out pokemon

--- backend-java-pokemon-interface/src/Pokemon.java
+++ backend-java-pokemon-interface/src/Pokemon.java
@@ -1,6 +1,34 @@
 public class Pokemon {
-
+    private final String name;
+    private final int level;
+    private int hp;
+    private final String food;
+    private final String sound;
     public Pokemon(String name, int level, int hp, String food, String sound){
+        this.name = name;
+        this.level = level;
+        this.hp = hp;
+        this.food = food;
+        this.sound = sound;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getSound() {
+        return sound;
     }
 }
