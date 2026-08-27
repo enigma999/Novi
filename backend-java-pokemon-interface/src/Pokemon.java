b53b2ecdc06a90663e03166bc100cbb9971cb067
Stan <stan.borst@storekeeper.com>
2026-08-27 17:17:45 +0100

    added pokemon classes and constructors

---
+++ backend-java-pokemon-interface/src/Pokemon.java
@@ -0,0 +1,6 @@
+public class Pokemon {
+
+    public Pokemon(String name, int level, int hp, String food, String sound){
+
+    }
+}
---
+++ backend-java-pokemon-interface/src/WaterPokemon.java
@@ -0,0 +1,5 @@
+public class WaterPokemon extends Pokemon {
+    public WaterPokemon(String name, int level, int hp, String food, String sound) {
+        super(name, level, hp, food, sound);
+    }
+}
